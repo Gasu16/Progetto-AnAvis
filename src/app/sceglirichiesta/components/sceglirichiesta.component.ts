4ea917e708f8d080cee1dc2f46d4cc1018c9e931
Gasu16 <rosarioma.grammatico@studenti.unicam.it>
2021-02-16 18:27:29 +0100

    Aggiunto questionario per scegliemergenza

--- src/app/sceglirichiesta/components/sceglirichiesta.component.ts
+++ src/app/sceglirichiesta/components/sceglirichiesta.component.ts
@@ -1,3 +1,4 @@
+import { SedeComponent } from './../../sede/components/sede.component';
 import { SceglirichiestaService } from './../services/sceglirichiesta.service';
 import { HttpClient } from '@angular/common/http';
 import { Emergenza } from './../../emergenza/emergenza';
@@ -15,6 +16,13 @@ export class SceglirichiestaComponent implements OnInit {
   citta!: string;
   gruppoSanguigno!: string;
   public scelta_emergenza_view!: string;
+  sedeComponent!: SedeComponent;
+  risposta1!: string;
+  risposta2!: string;
+  risposta3!: string;
+  risposta4!: string;
+  risposta5!: string;
+  risultato_quiz!: string;
 
   constructor(
     private sceglirichiestaService: SceglirichiestaService,
@@ -22,12 +30,38 @@ export class SceglirichiestaComponent implements OnInit {
   ) { }
 
 
+  gestisciQuiz(): string{
+    
+    console.log(this.risposta1);
+    console.log(this.risposta2);
+    console.log(this.risposta3);
+    console.log(this.risposta4);
+    console.log(this.risposta5);
+//    this.risultato_quiz = (this.risposta1 && this.risposta2 && this.risposta3 && this.risposta4 && this.risposta5);
+    if(
+      (this.risposta1 === "true") ||
+      (this.risposta2 === "true") ||
+      (this.risposta3 === "true") ||
+      (this.risposta4 === "true") ||
+      (this.risposta5 === "true")
+    ){
+      console.log("TEST fallito");
+      return this.risultato_quiz;
+    }
+    console.log("Risultato quiz: " + this.risultato_quiz);
+
+    this.scelta_emergenza_view = '1';
+    console.log("Questionario finito");
+    return this.risultato_quiz;
+  }
+
+
   elencaEmergenze(): void{
     this.sceglirichiestaService.scegliEmergenze(this.citta).subscribe((data: Emergenza[]) => {
       console.log(data);
       this.emergenze = data;
     });
-    this.scelta_emergenza_view = '1';
+    this.scelta_emergenza_view = '2';
   }
 
 
